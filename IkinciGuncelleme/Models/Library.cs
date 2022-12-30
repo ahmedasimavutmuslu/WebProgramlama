7a0fc7b80aa495a31873b33acb11b1e5dd908995
ahmedasimavutmuslu <96369028+ahmedasimavutmuslu@users.noreply.github.com>
2022-12-30 19:29:23 +0300

    Görünüm ve Veritabanı Bağlantısı
    Bootstrap ile beraber mağazanın yapılmaya başlanması, veritabanının code first yaklaşımıyla oluşturulması.

---
+++ IkinciGuncelleme/Models/Library.cs
@@ -0,0 +1,7 @@
+﻿namespace WebProje.Models
+{
+    public class Library
+    {
+        //Satın alma ve kütüphane görüntüleme için kullanılabilir. Muhtemelen sadece kütüphane için kullanılacak.
+    }
+}
